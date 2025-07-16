aa285ebb2b2ab95119a3f3638d83b14e30e590f6
Nikhil-4566 <nikhil.tr.mdb@gmail.com>
2025-07-16 14:11:55 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './common/Header'
+import Footer from './common/Footer'
+import Banner from './home/banner'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Banner/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
