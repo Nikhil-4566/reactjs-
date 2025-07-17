b63ff4bc744883088765083e38939f00c09e9e24
manishtaak17 <manishtaak@themadbrains.com>
2025-07-17 14:42:38 +0530

    chnages

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,8 +1,28 @@
-
+import { Link } from "react-router-dom"
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <header className="px-[60px] py-[35px] flex items-center justify-between ">
+        <div>
+          <div className="w-full max-w-[173px]">
+            <Link to="/">
+              <img src="/images/logo.png" alt=""  />
+            </Link>
+          </div>
+        </div>
+        <div>
+          <nav className="flex items-center justify-between gap-[30px]">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/howitwork">How It Works</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/Contact">Contact</Link>
+          </nav>
+        </div>
+        <div>
+        </div>
+      </header>
+    </>
   )
 }
-
 export default Header
--- src/home/Banner.jsx
+++ src/home/Banner.jsx
@@ -2,7 +2,7 @@
 const Banner = () => {
   return (
     <>
-    <div className="text-3xl font-bold  text-black">banner</div>
+    <div className="text-3xl font-bold  text-black font-roboto">banner</div>
     </>
   )
 }
